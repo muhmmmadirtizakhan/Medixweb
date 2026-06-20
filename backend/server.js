e7f9f5d3bb6b4a4750a9d157d2c5d41303777f3a
MUHAMMAD IRTIZA KHAN <irtizakhan844@gmail.com>
2026-06-20 17:05:11 +0500

    Update CORS options for preflight requests

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.options('/(.*)', cors(corsOptions))
 app.use(express.json())
 app.use('/api/chatbot', chatbotRoute)
 app.use('/api/users', usersRoute)
